48cb075f0c788b5924563e22f2a7a1debddabdfc
DESKTOP-3T85H9P\Adrian <csi23-awanuer@colegioaltair.net>
2024-04-22 13:52:09 +0200

    Agregar archivos de proyecto.

---
+++ ejercicio12listas/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ejercicio12listas
+{
+    class program
+    {
+        public static void Main(String[] args) {
+        
+            Queue<String> cola = new Queue<String>();
+
+            cola.Enqueue("Cliente 1");
+            cola.Enqueue("Cliente 2");
+            cola.Enqueue("Cliente 3");
+
+            //Primero en entrar
+            Console.WriteLine(cola.Peek());
+            //Ha salido de la tienda
+            cola.Dequeue();
+            //Ahora va el siguien
+            Console.WriteLine(cola.Peek());
+
+        }
+    }
+}
